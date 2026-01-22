4dc8bac05bf6b27ff919b22dce14f38aba7ab0dc
Nicolas Cusan <nicolas-cusan@users.noreply.github.com>
2026-01-22 15:48:49 +0100

    ss6 compatibility

--- src/MetaTagsExtension.php
+++ src/MetaTagsExtension.php
@@ -1,62 +1,93 @@
 <?php
+
 namespace Arillo\MetaTags;
 
-use SilverStripe\ORM\DataExtension;
-use Silverstripe\Assets\Image;
+use SilverStripe\Core\Extension;
+use SilverStripe\Assets\Image;
 use SilverStripe\Forms\FieldList;
 use SilverStripe\Forms\TextField;
 use SilverStripe\AssetAdmin\Forms\UploadField;
 use SilverStripe\Core\Config\Config;
 use SilverStripe\View\SSViewer;
-use SilverStripe\CMS\Controllers\ContentController;
 use SilverStripe\i18n\i18n;
 
-class MetaTagsExtension extends DataExtension
+class MetaTagsExtension extends Extension
 {
     const TAB_NAME = 'Root.Meta';
 
-    private static $db = [
+    private static array $db = [
         'MetaTitle' => 'Varchar(255)',
     ];
 
-    private static $has_one = [
+    private static array $has_one = [
         'MetaImage' => Image::class,
     ];
 
-    private static $owns = ['MetaImage'];
+    private static array $owns = ['MetaImage'];
 
+    /**
+     * Prepare CMS fields for meta tags.
+     * Can be called statically for manual field setup or used internally by updateCMSFields.
+     *
+     * @param FieldList $fields The FieldList to modify
+     * @param string $tab The tab to add fields to
+     * @param object|null $owner Optional owner object for Fluent decoration
+     * @return FieldList
+     */
     public static function prepare_cms_fields(
         FieldList $fields,
-        string $tab = self::TAB_NAME
+        string $tab = self::TAB_NAME,
+        $owner = null
     ) {
-        if ($medaDesc = $fields->dataFieldByName('MetaDescription')) {
+        // Move MetaDescription from Metadata accordion to specified tab
+        if ($metaDesc = $fields->dataFieldByName('MetaDescription')) {
             $fields->removeByName('Metadata');
             $fields->removeByName('MetaDescription');
+
+            $metaTitleField = TextField::create(
+                'MetaTitle',
+                _t(__CLASS__ . '.MetaTitle', 'Meta title')
+            )->setDescription('Custom page title for search engines and social sharing');
+
+            $metaDesc->setTitle(
+                _t(__CLASS__ . '.MetaDescription', 'Meta description')
+            );
+
             $fields->addFieldsToTab($tab, [
-                TextField::create(
-                    'MetaTitle',
-                    _t(__CLASS__ . '.MetaTitle', 'Meta title')
-                ),
-                $medaDesc->setTitle(
-                    _t(__CLASS__ . '.MetaDescription', 'Meta description')
-                ),
+                $metaTitleField,
+                $metaDesc,
             ]);
+
+            // Apply Fluent decoration if owner is provided and has FluentExtension
+            if ($owner && $owner->hasExtension('TractorCow\\Fluent\\Extension\\FluentExtension')) {
+                $fluentExt = $owner->getExtensionInstance('TractorCow\\Fluent\\Extension\\FluentExtension');
+                if ($fluentExt && method_exists($fluentExt, 'updateFluentCMSField')) {
+                    // Remove class first to force re-decoration (in case field was moved)
+                    $metaTitleField->removeExtraClass('fluent__localised-field');
+                    $metaDesc->removeExtraClass('fluent__localised-field');
+
+                    $fluentExt->updateFluentCMSField($metaTitleField);
+                    $fluentExt->updateFluentCMSField($metaDesc);
+                }
+            }
         }
+
+        // Add MetaImage field
+        $upload = UploadField::create(
+            'MetaImage',
+            _t(__CLASS__ . '.MetaImage', 'Meta image')
+        )->setDescription('Image for social sharing (Open Graph). Recommended: 1200x630px');
+        $upload->getValidator()->setAllowedExtensions(['jpeg', 'jpg', 'png']);
+        $upload->setFolderName('MetaImages');
+
+        $fields->addFieldToTab($tab, $upload);
 
         return $fields;
     }
 
     public function updateCMSFields(FieldList $fields)
     {
-        $upload = UploadField::create(
-            'MetaImage',
-            _t(__CLASS__ . '.MetaImage', 'Meta image')
-        );
-        $upload->getValidator()->setAllowedExtensions(['jpeg', 'jpg', 'png']);
-
-        $upload->setFolderName('MetaImages');
-        $fields->addFieldToTab(self::TAB_NAME, $upload);
-        return $fields;
+        self::prepare_cms_fields($fields, self::TAB_NAME, $this->owner);
     }
 
     public function MetaTagsX()
